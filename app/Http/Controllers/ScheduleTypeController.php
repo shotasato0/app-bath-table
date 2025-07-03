4316112912030a822ad43fe5ae26ba11f89af91d
佐藤翔太 <shota19830706@gmail.com>
2025-07-04 08:17:18 +0900

    [add]スケジュール種別用のマイグレーション・モデル・コントローラーを追加
    介護施設のスケジュール種別（ScheduleType）に関する基本的なリソース操作を実装するため、マイグレーション・モデル・コントローラーを作成しました。

---
+++ app/Http/Controllers/ScheduleTypeController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ScheduleType;
+use Illuminate\Http\Request;
+
+class ScheduleTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ScheduleType $scheduleType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ScheduleType $scheduleType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ScheduleType $scheduleType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ScheduleType $scheduleType)
+    {
+        //
+    }
+}
