818f5dda16a53a140b499f5e8701901a6f2f0629
佐藤翔太 <shota19830706@gmail.com>
2025-07-04 09:07:52 +0900

    update]departmentsテーブルにdepartment_nameカラムとインデックスを追加
    部署名の保存と検索性能向上のため、department_nameカラムの追加およびインデックスを設定しました。

--- database/migrations/2025_07_03_230819_create_departments_table.php
+++ database/migrations/2025_07_03_230819_create_departments_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('departments', function (Blueprint $table) {
             $table->id();
+            $table->string('department_name');
             $table->timestamps();
+            
+            $table->index('department_name');
         });
     }
 
