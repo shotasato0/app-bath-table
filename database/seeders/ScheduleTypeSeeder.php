4754438eb780843dd6147e44a48deedfb6f7eb12
佐藤翔太 <shota19830706@gmail.com>
2025-07-05 09:41:14 +0900

    [add] スケジュール種別データを初期投入するSeederを実装
    各種スケジュール（入浴、会議、イベントなど）を管理画面等で選択できるようにするため、初期データをSeederに追加しました。

--- database/seeders/ScheduleTypeSeeder.php
+++ database/seeders/ScheduleTypeSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\ScheduleType;
 
 class ScheduleTypeSeeder extends Seeder
 {
@@ -12,6 +13,51 @@ class ScheduleTypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $scheduleTypes = [
+            [
+                'type_name' => '入浴',
+                'color_code' => '#FF5733',
+                'is_active' => true
+            ],
+            [
+                'type_name' => '会議',
+                'color_code' => '#3498DB',
+                'is_active' => true
+            ],
+            [
+                'type_name' => 'イベント',
+                'color_code' => '#9B59B6',
+                'is_active' => true
+            ],
+            [
+                'type_name' => 'レクリエーション',
+                'color_code' => '#E74C3C',
+                'is_active' => true
+            ],
+            [
+                'type_name' => '個別対応',
+                'color_code' => '#F39C12',
+                'is_active' => true
+            ],
+            [
+                'type_name' => '送迎',
+                'color_code' => '#27AE60',
+                'is_active' => true
+            ],
+            [
+                'type_name' => '食事',
+                'color_code' => '#E67E22',
+                'is_active' => true
+            ],
+            [
+                'type_name' => 'その他',
+                'color_code' => '#95A5A6',
+                'is_active' => true
+            ],
+        ];
+
+        foreach ($scheduleTypes as $scheduleType) {
+            ScheduleType::create($scheduleType);
+        }
     }
 }
