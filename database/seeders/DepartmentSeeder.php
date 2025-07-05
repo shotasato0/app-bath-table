70a54a51dcab5ef772f075babc1c4d42ba984479
佐藤翔太 <shota19830706@gmail.com>
2025-07-05 09:41:41 +0900

    [add] 部署名の初期データをSeederに追加
    介護業務に必要な各部署（事務所、デイサービス、訪問介護など）を選択可能にするため、Seederに初期データを追加しました。

--- database/seeders/DepartmentSeeder.php
+++ database/seeders/DepartmentSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Department;
 
 class DepartmentSeeder extends Seeder
 {
@@ -12,6 +13,18 @@ class DepartmentSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $departments = [
+            ['department_name' => '事務所'],
+            ['department_name' => 'デイサービス'],
+            ['department_name' => '訪問介護'],
+            ['department_name' => '居宅介護支援'],
+            ['department_name' => '看護部'],
+            ['department_name' => '栄養管理'],
+            ['department_name' => '相談支援'],
+        ];
+
+        foreach ($departments as $department) {
+            Department::create($department);
+        }
     }
 }
