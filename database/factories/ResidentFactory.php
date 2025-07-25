1a32bb092f73ad6400ceaeef3cb3154466e4feb2
佐藤翔太 <shota19830706@gmail.com>
2025-07-26 08:35:35 +0900

    [update] Residentファクトリに属性値の多様化と追加項目のダミーデータ生成を実装
    性別の選択肢をmale・female・otherに拡張し、age・room_number・care_level・status・special_requirements・primary_staff_idなど、現場に即した属性を追加することで、より現実的なテストデータを生成可能にしました。

--- database/factories/ResidentFactory.php
+++ database/factories/ResidentFactory.php
@@ -18,9 +18,15 @@ class ResidentFactory extends Factory
     {
         return [
             'name' => $this->faker->name(),
-            'gender' => $this->faker->randomElement(['M', 'F']),
+            'gender' => $this->faker->randomElement(['male', 'female', 'other']),
             'birth_date' => $this->faker->dateTimeBetween('-100 years', '-65 years')->format('Y-m-d'),
+            'age' => $this->faker->numberBetween(60, 100),
+            'room_number' => $this->faker->optional()->regexify('[A-Z][0-9]{3}'),
+            'care_level' => $this->faker->optional()->randomElement(['軽度', '中度', '重度']),
+            'status' => $this->faker->randomElement(['active', 'inactive', 'medical_care', 'temporary_leave']),
             'medical_notes' => $this->faker->optional()->paragraph(),
+            'special_requirements' => $this->faker->optional()->sentence(),
+            'primary_staff_id' => null,
         ];
     }
 }
