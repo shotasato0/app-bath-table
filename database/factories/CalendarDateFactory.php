2a529b3cce6eec95694c2b82d8819554cc095a21
佐藤翔太 <shota19830706@gmail.com>
2025-07-26 08:38:05 +0900

    [update] CalendarDateファクトリ内のfaker呼び出しをfake()に統一
    $this->fakerからfake()への記述変更を行いました。

--- database/factories/CalendarDateFactory.php
+++ database/factories/CalendarDateFactory.php
@@ -17,11 +17,11 @@ class CalendarDateFactory extends Factory
     public function definition(): array
     {
         return [
-            'calendar_date' => $this->faker->dateTimeBetween('now', '+1 year')->format('Y-m-d'),
-            'day_of_week' => $this->faker->numberBetween(0, 6),
-            'is_holiday' => $this->faker->boolean(20),
-            'holiday_name' => $this->faker->optional()->words(2, true),
-            'notes' => $this->faker->optional()->sentence(),
+            'calendar_date' => fake()->dateTimeBetween('now', '+1 year')->format('Y-m-d'),
+            'day_of_week' => fake()->numberBetween(0, 6),
+            'is_holiday' => fake()->boolean(20),
+            'holiday_name' => fake()->optional()->words(2, true),
+            'notes' => fake()->optional()->sentence(),
         ];
     }
 }
