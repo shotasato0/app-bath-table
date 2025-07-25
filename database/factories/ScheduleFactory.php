4853e7d79e6d12d474990ea9a4b3b98013258f3c
佐藤翔太 <shota19830706@gmail.com>
2025-07-26 08:31:34 +0900

    [update] Scheduleファクトリに関連モデル・時間帯・ダミーデータ生成処理を追加
    スケジュールデータのテスト作成を容易にするため、title・description・start_time・end_timeにfakerを活用したダミーデータ生成ロジックを追加し、CalendarDate・ScheduleType・Residentとの関連もファクトリで自動生成されるよう設定しました。

--- database/factories/ScheduleFactory.php
+++ database/factories/ScheduleFactory.php
@@ -3,6 +3,9 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\CalendarDate;
+use App\Models\ScheduleType;
+use App\Models\Resident;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Schedule>
@@ -16,8 +19,17 @@ class ScheduleFactory extends Factory
      */
     public function definition(): array
     {
+        $startHour = $this->faker->numberBetween(6, 20);
+        $endHour = $startHour + $this->faker->numberBetween(1, 3);
+        
         return [
-            //
+            'date_id' => CalendarDate::factory(),
+            'title' => $this->faker->randomElement(['入浴サービス', 'ランチタイム', 'レクリエーション', '医療ケア', 'リハビリ']),
+            'description' => $this->faker->optional()->sentence(),
+            'start_time' => sprintf('%02d:00', $startHour),
+            'end_time' => sprintf('%02d:00', min($endHour, 23)),
+            'schedule_type_id' => ScheduleType::factory(),
+            'resident_id' => Resident::factory(),
         ];
     }
 }
