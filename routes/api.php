7ba5f6fda59a203618b847d9ae29e8551aba632d
佐藤翔太 <shota19830706@gmail.com>
2025-07-31 07:31:47 +0900

    [add] ResidentControllerをAPIルーティングで使用するためにインポートを追加
    利用者情報に関するAPIエンドポイントを有効にするため、ResidentControllerのuse宣言をルーティングファイルに追加しました。

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ScheduleController;
 use App\Http\Controllers\ScheduleTypeController;
+use App\Http\Controllers\ResidentController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
