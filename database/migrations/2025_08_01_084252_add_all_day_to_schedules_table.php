c686de12970f6c6f09d997c532ecec59cc0a41e8
佐藤翔太 <shota19830706@gmail.com>
2025-08-01 08:44:13 +0900

    *update downメソッドにall_dayカラムの削除処理を追加
    マイグレーションのロールバック時にall_dayカラムが正しく削除されるよう、dropColumnを追加しました。

--- database/migrations/2025_08_01_084252_add_all_day_to_schedules_table.php
+++ database/migrations/2025_08_01_084252_add_all_day_to_schedules_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('schedules', function (Blueprint $table) {
-            //
+            $table->dropColumn('all_day');
         });
     }
 };
