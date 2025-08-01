058270539793a6b87411eb1fdccfb2a3cc694b21
佐藤翔太 <shota19830706@gmail.com>
2025-08-01 13:33:55 +0900

    [update] パフォーマンス最適化のためmemoをインポートに追加
    コンポーネントの再レンダリング抑制を目的に、React.memoの利用に備えてmemoをインポートしました。

--- resources/js/Components/Calendar/CalendarDay.jsx
+++ resources/js/Components/Calendar/CalendarDay.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, memo } from 'react';
 import { format } from 'date-fns';
 import ScheduleModal from './ScheduleModal';
 
